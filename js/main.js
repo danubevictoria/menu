7f7d77b5e6196b40d124cfb4d4b7c10a7c65e10a
unknown <danubevictoria@gmail.com>
2017-12-08 17:23:03 -0800

    updated

--- js/main.js
+++ js/main.js
@@ -21,14 +21,30 @@ $(document).ready(function(){
   $('#menu-right').on('click', setMenuRight);
 
   function setMenuRight() {
+    removeMenuChanges();
     $('.menu').addClass('menu-right');
     $('.window').addClass('window-menu-right');
   };
 
-  $('#menu-left').on('click', removeMenuRight);
+  $('#menu-left').on('click', setMenuLeft);
 
-  function removeMenuRight() {
+  function removeMenuChanges() {
+    let menu = document.getElementById('resizable');
+
+    if (menu.style.right) {
+      menu.style.right = null;
+    }
+    if (menu.style.left) {
+      menu.style.left = null;
+    }
+
+    $('.menu').removeClass('menu-left');
     $('.menu').removeClass('menu-right');
     $('.window').removeClass('window-menu-right');
+  };
+
+  function setMenuLeft() {
+    removeMenuChanges();
+    $('.menu').addClass('menu-left');
   };
 });
